1e338fc8c8dce4585931c77dc661ea8f28829e62
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2025-02-11 10:12:13 -0800

    added new physics material and added new script
    two new physics materials:
    --Rough
    --Smooth
    
    New script added(Player)
    --Player script added to control the movement of player model (A move left) and D move right

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
 
     void Update()
@@ -13,5 +14,16 @@ public class InputManager : MonoBehaviour
             print("SpacePress");
 
         }
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A)) {
+            input += Vector2.left;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+
+        }
+        OnMove?.Invoke(input);
     }
 }
